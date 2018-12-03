a762c5ea2e66b69e84bebace7d0738ef34af32bf
Justin <schujustin@gmail.com>
2018-12-03 18:36:20 -0500

    Added character reading capabilities

--- shell.c
+++ shell.c
@@ -1,7 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char *read_line(void);
+#define SH_RL_BUFSIZE 1024
+
+char *read_line(void) {
+    int buff_size = SH_RL_BUFSIZE;
+    int position = 0;
+    char *buffer = malloc(sizeof(char) *buff_size);
+    int c;
+
+    if(!buffer) {
+        fprintf(stderr, "shell: allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // Read a character
+    while (1) {
+        c = getchar();
+
+        // IF EOF replace with a null char and return
+        if (c == EOF || c == '\n') {
+            buffer[position] = '\0';
+            return buffer;
+        } else {
+            buffer[position] = c;
+        }
+        position++;
+
+        // If we have exceeded the buffer, reallocate
+        if (position >= buff_size) {
+            buff_size += SH_RL_BUFSIZE;
+            buffer = realloc(buffer, buff_size);
+
+            if (!buffer) {
+                fprintf(stderr, "shell: allocate error\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
+}
+
 char **split_line(char (*line));
 int shell_execute(char **args);
 
