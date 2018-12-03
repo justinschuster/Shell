0832647974e84c59caa24fdba5f48dbda35d40b8
Justin <schujustin@gmail.com>
2018-12-03 18:46:52 -0500

    Now can parse commands into tokens

--- shell.c
+++ shell.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define SH_RL_BUFSIZE 1024
+#define SH_TOK_BUFSIZE 64
+#define SH_TOK_DELIM " \t\r\n\a"
 
 char *read_line(void) {
     int buff_size = SH_RL_BUFSIZE;
@@ -40,7 +43,38 @@ char *read_line(void) {
     }
 }
 
-char **split_line(char (*line));
+char **split_line(char (*line)) {
+    int buff_size = SH_TOK_BUFSIZE, position = 0;
+    char **tokens = malloc(buff_size * sizeof(char *));
+    char *token;
+
+    if (!tokens) {
+        fprintf(stderr, "shell: allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    token = strtok(line, SH_TOK_DELIM);
+    while (token != NULL) {
+        tokens[position] = token;
+        position++;
+
+        if (position >= buff_size) {
+            buff_size += SH_TOK_BUFSIZE;
+            tokens = realloc(tokens, buff_size * sizeof(char *));
+
+            if (!tokens) {
+                fprintf(stderr, "shell: allocation error\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        token = strtok(NULL, SH_TOK_DELIM);
+    }
+
+    tokens[position] = NULL;
+    return tokens;
+}
+
 int shell_execute(char **args);
 
 void shell_loop(void) {
