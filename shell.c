d7982fc21519d3cdfc11c79e102332a0112388f0
Justin <schujustin@gmail.com>
2018-12-03 18:28:19 -0500

    Made shell_loop, the basic loop

--- shell.c
+++ shell.c
@@ -1,3 +1,30 @@
-int main (int argc, char *argv[]) {
+#include <stdio.h>
+#include <stdlib.h>
 
+char *read_line(void);
+char **split_line(char (*line));
+int shell_execute(char **args);
+
+void shell_loop(void) {
+    char *line;
+    char **args;
+    int status;
+
+    do {
+        printf("prompt> ");
+        line = read_line();
+        args = split_line(line);
+        status = shell_execute(args);
+
+        free(line);
+        free (args);
+    } while (status);
+}
+
+int main (int argc, char *argv[]) {
+    
+    shell_loop(); 
+      
+
+    return 0;
 }
