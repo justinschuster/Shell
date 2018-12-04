fe1a388519c4c35a21951d9c9ac908177bc65de2
Justin <schujustin@gmail.com>
2018-12-03 19:03:00 -0500

    wrote shell_cd function

--- shell.c
+++ shell.c
@@ -40,6 +40,17 @@ int shell_num_builtins() {
 /*
  * Builtin function implementations
  */
+int shell_cd(char **args) {
+    if (args[1] == NULL) {
+        fprintf(stderr, "shell: expected argument to \"cd\"\n");
+    } else {
+        if (chdir(args[1]) != 0) {
+            perror("shell");
+        }
+    }
+
+    return 1;
+}
 
 int launch_shell(char **args) {
     pid_t pid, wpid;
