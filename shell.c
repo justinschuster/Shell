ae0cf0b959a806b98e1896958ea7ae62f77b5ae6
Justin <schujustin@gmail.com>
2018-12-06 15:28:44 -0500

    Switch around the shell_execute function so we properly get background flag

--- shell.c
+++ shell.c
@@ -70,14 +70,13 @@ int shell_quit(char **args) {
     return 0;
 }
 
-int launch_shell(char **args) {
+int launch_shell(char **args, int background) {
     pid_t pid, wpid;
-    int status;
-    int background = 0;
+    int status; 
 
     pid = fork();
     if (pid == 0) {
-        // Child process 
+        // Child process  
             
         if (execvp(args[0], args) == -1) {
             perror("shell");
@@ -168,20 +167,54 @@ char **split_line(char (*line)) {
 }
 
 int shell_execute(char **args) {
-    int i;
+    int background = 0;
+    int i = 0;
+    int j = 0;
+
+    char *args_copy[256];
 
     if (args[0] == NULL) {
         // Empty command was entered
         return 1;
     }
+    
+    while (args[j] != NULL) {
+        if ((strcmp(args[j], "&")) == 0) {
+            break;
+        }
+        
+        args_copy[j] = args[j];
+        j++;
+    } 
 
-    for (i = 0; i < shell_num_builtins(); i++) {
+    /*for (i = 0; i < shell_num_builtins(); i++) {
         if (strcmp(args[0], builtin_str[i]) == 0) {
             return (*builtin_func[i])(args);
         }
-    }
+    }*/
 
-    return launch_shell(args);
+    // quit end shell 
+    if (strcmp(args[0], "quit") == 0) return shell_quit(args);
+    // help returns list of builtin functions
+    else if (strcmp(args[0], "help") == 0) return shell_help(args);
+    // cd changes the directory
+    else if (strcmp(args[0], "cd") == 0) return shell_cd(args);
+    // args[0] is not a builtin function
+    else {
+        while (args[i] != NULL && background == 0) {
+            if (strcmp(args[i], "&") == 0) {
+                // Change background flag
+                // Because & is last thing on command line end loop
+                background = 1;
+            }
+
+            i++;
+        }
+        
+    }
+    args_copy[i] = NULL;
+
+    return launch_shell(args, background);
 }
 
 void interactive_mode(void) {
