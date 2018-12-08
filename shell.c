b505de96952d4aba3cdb83b317fa2a5a255deb71
Justin <schujustin@gmail.com>
2018-12-08 17:12:24 -0500

    put in foreground and background

--- shell.c
+++ shell.c
@@ -75,6 +75,41 @@ int job_is_completed (job *j) {
     return 1;
 }
 
+/* Put job j in the foreground, If cont is nonzero
+ * restore the saved terminal nodes and send the process group a 
+ * SIGCONT signal to wake it up before we block. */
+void put_job_in_foreground(job *j, int cont) {
+    /* Put the job into the foreground */
+    tcsetpgrp(shell_terminal, j->pgid);
+
+    /* Send the job a continue signal, if necessary */
+    if (cont) {
+        tcsetattr(shell_terminal, TCSADRAIN, &j->tmodes);
+        if (kill(- j->pgid, SIGCONT) < 0) {
+            perror("kill (SIGCONT)");
+        }
+    }
+
+    /* Wait for it to report */
+    wait_for_job(j);
+
+    /* Put the shell back in the foreground */
+    tcsetpgrp(shell_terminal, shell_pgid);
+
+    /* Restore the shell's terminal modes */
+    tcgetattr(shell_terminal, &j->tmodes);
+    tcsetattr(shell_terminal, TCSADRAIN, &shell_tmodes);
+}
+
+void put_job_in_background(job *j, int cont) {
+    /* Send the job a continue signal if necessary */
+    if (cont) {
+        if (kill (-j->pgid, SIGCONT) < 0) {
+            perror("kill (SIGCONT)");
+        }
+    }
+}
+
 /* Make sure the shell is running interactively as the foreground job before proceeding */
 void init_shell () {
     /* See if we are running interactively */
