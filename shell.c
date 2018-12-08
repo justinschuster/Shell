5ea8d6f1d240771acc96c1926d068f0924dcd685
Justin <schujustin@gmail.com>
2018-12-08 17:41:11 -0500

    Can now handle stopped jobs

--- shell.c
+++ shell.c
@@ -1,4 +1,4 @@
-#define _POSIX_SOURCE
+#define _POSIX_SOURCE 200112L 
 
 #include <sys/types.h>
 #include <unistd.h>
@@ -6,6 +6,9 @@
 #include <stdio.h>
 #include <termios.h>
 #include <signal.h>
+#include <sys/wait.h>
+#include <errno.h>
+#include <fcntl.h>
 
 /* A process object is a single process */
 typedef struct process {
@@ -75,6 +78,101 @@ int job_is_completed (job *j) {
     return 1;
 }
 
+int mark_process_status(pid_t pid, int status) {
+    job *j;
+    process *p;
+
+    if (pid > 0) {
+        /* Update the record for the process */
+        for (j = first_job; j; j = j->next) {
+            for (p = j->first_process; p; p = p->next) {
+                if (p->pid == pid) {
+                    p->status = status;
+                    if (WIFSTOPPED(status)) {
+                        p->stopped = 1;
+                    } else {
+                        p->completed = 1;
+                        if (WIFSIGNALED(status)) {
+                            fprintf(stderr, "%d: Terminated by signal %d.\n", (int)pid, WTERMSIG (p->status));
+                        }
+                    }
+                    return 0;
+                }
+            }
+        }
+        fprintf(stderr, "No child process %d.\n", pid);
+        return -1;
+    } else if (pid == 0 || errno == ECHILD) {
+        /* No process ready to report */
+        return -1;
+    } else {
+        /* Other weird errors */
+        perror("waitpid");
+        return -1;
+    }
+}
+
+/* Check for processes that have status information availabe without blocking */
+void update_status(void) {
+    int status;
+    pid_t pid;
+
+    do {
+        pid = waitpid(WAIT_ANY, &status, WUNTRACED(WNOHANG));
+    } while (!mark_process_status(pid, status));
+}
+
+/* Check for processes that have status information available
+ * blocking until all processes in the given job have reported */
+void wait_for_job(job *j) {
+    int status;
+    pid_t pid;
+
+    do {
+        pid = waitpid(WAIT_ANY, &status, WUNTRACED);
+    } while (!mark_process_status(pid, status) && !job_is_stopped(j) && !job_is_completed(j));
+}
+
+void format_job_info(job *j, const char *status) {
+    fprintf(stderr, "%ld (%s): %s\n", (long)j->pgid, status, j->command);
+}
+
+/* Notify the user about the stopped or terminated jobs.
+ * Delete terminated jobs from the active job list */
+void do_job_notification(void) {
+    job *j, *jlast, *jnext;
+    process *p;
+
+    /* Update the status information for the child process */
+    update_status();
+
+    jlast = NULL;
+    for (j = first_job; j; j = jnext) {
+        jnext = j->next;
+
+        /* If all proceesses have completed tell the user the job has
+         * completed and delete it from the list of active jobs */
+        if (job_is_completed(j)) {
+            format_job_info(j, "completed");
+            if (jlast) {
+                jlast->next = jnext;
+            } else {
+                first_job = jnext;
+            }
+            free_job();
+        } else if (job_is_stopped(j) && !j->notified) {
+            /* Notify the user about stopped jobs  marking them so that
+            * we won't do this more than once */
+            format_job_info(j, "stopped");
+            j->notified = 1;
+            jlast = j; 
+        } else {
+            /* Don't say anything about jobs that are still running */
+            jlast = j;
+        }
+    }
+}
+
 /* Put job j in the foreground, If cont is nonzero
  * restore the saved terminal nodes and send the process group a 
  * SIGCONT signal to wake it up before we block. */
@@ -107,6 +205,27 @@ void put_job_in_background(job *j, int cont) {
         if (kill (-j->pgid, SIGCONT) < 0) {
             perror("kill (SIGCONT)");
         }
+    }
+}
+
+/* Mark a stopped job J as being running again */
+void mark_job_as_running(job *j) {
+    process *p;
+
+    for (p = j->first_process; p; p = p->next) {
+        p->stopped = 0;
+    }
+
+    j->notified = 0;
+}
+
+/* Continue the job J */
+void continue_job(job *j, int foreground) {
+    mark_job_as_running(j);
+    if (foreground) {
+        put_job_in_foreground(j, 1);
+    } else {
+        put_job_in_background(j, 1);
     }
 }
 
